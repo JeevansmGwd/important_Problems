37ee719fb18c1f33f75dec20e51f814b9932ca18
JeevansmGwd <119862162+JeevansmGwd@users.noreply.github.com>
2023-01-12 12:38:27 +0530

    Update Hours and minutes as input,and calculates the total number of minutes.c

--- Hours and minutes as input,and calculates the total number of minutes.c
+++ Hours and minutes as input,and calculates the total number of minutes.c
@@ -2,7 +2,9 @@
 void main()
 {
     int a,b,c;
+    printf("Enter hours and minutes\n");
     scanf("%d%d",&a,&b);
     c=a*60+b;
+    //converting hours in minutes
     printf("total minute=%d",c);
 }
